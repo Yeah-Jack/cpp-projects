e25cbcfbd8c5e7b96a94949ed84d825e661377bb
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-16 17:28:32 +0200

    Refactor main functions for improved readability and consistency

--- Table/main.cpp
+++ Table/main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main() {
-    cout << "Test 1: " << (2+3 == 5 ? "Bestanden" : "Fehlgeschlagen") << endl;
-    cout << "Test 2: " << (6 + 4 == 10 ? "Bestanden" : "Fehlgeschlagen") << endl;
-    cout << "Test 3: " << (10 + 20 == 30 ? "Bestanden" : "Fehlgeschlagen") << endl;
-    cout << "Test 4: " << (5 + 3 == 7 ? "Bestanden" : "Fehlgeschlagen") << endl;
-    cout << "Test 5: " << (100 + 200 == 250 ? "Bestanden" : "Fehlgeschlagen") << endl;
+  cout << "Test 1: " << (2 + 3 == 5 ? "Bestanden" : "Fehlgeschlagen") << endl;
+  cout << "Test 2: " << (6 + 4 == 10 ? "Bestanden" : "Fehlgeschlagen") << endl;
+  cout << "Test 3: " << (10 + 20 == 30 ? "Bestanden" : "Fehlgeschlagen")
+       << endl;
+  cout << "Test 4: " << (5 + 3 == 7 ? "Bestanden" : "Fehlgeschlagen") << endl;
+  cout << "Test 5: " << (100 + 200 == 250 ? "Bestanden" : "Fehlgeschlagen")
+       << endl;
 
-    return 0;
+  return 0;
 }
--- VariableSwitcher/main.cpp
+++ VariableSwitcher/main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+  int zahl1, zahl2, temp;
 
-int main()
-{
-    int zahl1, zahl2, temp;
+  std::cout << "Gebe Zahl 1 ein: " << std::endl;
+  std::cin >> zahl1;
+  std::cout << "Gebe Zahl 2 ein: " << std::endl;
+  std::cin >> zahl2;
 
-    std::cout << "Gebe Zahl 1 ein: " << endl;
-    std::cin >> zahl1;
-    std::cout << "Gebe Zahl 2 ein: " << endl;
-    std::cin >> zahl2;
+  temp = zahl1;
+  zahl1 = zahl2;
+  zahl2 = temp;
 
-    temp = zahl1;
-    zahl1 = zahl2;
-    zahl2 = temp;
+  std::cout << "Neue Zahl 1: " << zahl1 << std::endl;
+  std::cout << "Neue Zahl 2: " << zahl2 << std::endl;
 
-    cout << "Neue Zahl 1: " << zahl1 << endl;
-    cout << "Neue Zahl 2: " << zahl2 << endl;
-
-    return 0;
+  return 0;
 }
