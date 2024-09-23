4a06b7f54715fe8090712868c38704349c0441d5
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-23 10:00:16 +0200

    Add German Bankgebühren program to calculate transaction fees based on monthly bookings

---
+++ German/Bankgebühren/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+
+	int Buchungen;
+	double Gebühren;
+
+	std::cout << "Wie viele Buchungen haben Sie im Monat getätigt? ";
+	std::cin >> Buchungen;
+
+	if (Buchungen <= 10) {
+		std::cout << "Sie zahlen 0€ Gebühren pro Transaktion." << std::endl;
+		Gebühren = 0;
+	} else if (Buchungen > 10 && Buchungen <= 20) {
+		Buchungen = Buchungen - 10;
+		std::cout << "Sie zahlen 0.15€ Gebühren pro Transaktion." << std::endl;
+		Gebühren = 0.15;
+	} else {
+		Buchungen = Buchungen - 10;
+		std::cout << "Sie zahlen 0.10€ Gebühren pro Transaktion." << std::endl;
+		Gebühren = 0.10;
+	}
+
+	std::cout << "Ihre Gebühren betragen: " << Buchungen * Gebühren << "€" << std::endl;
+
+	return 0;
+}
