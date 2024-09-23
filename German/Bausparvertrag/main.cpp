a73f462616fa9265dbd33ff905145b38c058cee0
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-23 09:33:31 +0200

    Add German implementation for Bausparvertrag program

---
+++ German/Bausparvertrag/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+  double Sparleistung, Prämie;
+  short Kinder;
+  bool Verheiratet;
+
+  std::cout << "Wie viel Euro möchten Sie monatlich sparen? ";
+  std::cin >> Sparleistung;
+
+  if (Sparleistung > 800) {
+    Sparleistung = 800;
+    std::cout << "Der Maximalsparbetrag beträgt 800 Euro und wurde deshalb "
+                 "auch auf 800 Euro geändert."
+              << std::endl;
+  }
+
+  std::cout << "Sind Sie verheiratet? (1 = Ja, 0 = Nein)" << std::endl;
+  std::cin >> Verheiratet;
+
+  if (Verheiratet == true) {
+    Prämie = Sparleistung * 0.07;
+  } else {
+    Prämie = Sparleistung * 0.14;
+  }
+
+  std::cout << "Wie viele Kinder haben Sie?" << std::endl;
+  std::cin >> Kinder;
+
+  Prämie += Kinder * 0.02 * Sparleistung;
+
+  std::cout << "Deine Prämie ist: " << Prämie << " Euro" << std::endl;
+
+  return 0;
+}
