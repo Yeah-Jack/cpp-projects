9e0728c4a01d16f65b8d7e391b98902a3a3f5c32
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-23 10:55:15 +0200

    Add German Wettervorhersage program to provide weather forecasts based on wind direction and air pressure

---
+++ German/Wettervorhersage/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+
+	int Windrichtung;
+	double Luftdruck;
+
+	std::cout << "Gebe die Windrichtung ein (1=Norden, 2=Osten, 3=Süden, 4=Westen)" << std::endl;
+	std::cin >> Windrichtung;
+
+	std::cout << "Gebe den Luftdruck in hPa ein:" << std::endl;
+	std::cin >> Luftdruck;
+
+	if (Luftdruck <= 1010)
+	{
+		switch (Windrichtung)
+		{
+			case 1:
+				std::cout << "Das Wetter im Norden ist veränderlich" << std::endl;
+				break;
+			case 2:
+				std::cout << "Das Wetter im Osten ist veränderlich" << std::endl;
+				break;
+			case 3:
+				std::cout << "Das Wetter im Süden ist veränderlich" << std::endl;
+				break;
+			case 4:
+				std::cout << "Das Wetter im Westen ist regnerisch" << std::endl;
+				break;
+		}
+	}
+	else if (Luftdruck > 1010)
+	{
+		switch (Windrichtung)
+		{
+			case 1:
+				std::cout << "Das Wetter im Norden ist gut" << std::endl;
+				break;
+			case 2:
+				std::cout << "Das Wetter im Osten ist gut" << std::endl;
+				break;
+			case 3:
+				std::cout << "Das Wetter im Süden ist gut" << std::endl;
+				break;
+			case 4:
+				std::cout << "Das Wetter im Westen ist veränderlich" << std::endl;
+				break;
+		}
+	}
+
+	return 0;
+}
