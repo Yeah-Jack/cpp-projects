a85c5ec44fc5d173abf73f2c420442b59be0ac0b
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-10-02 20:27:47 +0200

    refactor: enhance Person struct and update print functionality

--- English/StructExercises/main.cpp
+++ English/StructExercises/main.cpp
@@ -5,25 +5,44 @@ struct Person {
   std::string name;
   int age;
   double height;
+  double weight;
 };
 
-void printPerson(Person &person);
+void printPerson(const Person &person);
+void updatePerson(Person &person, int newAge, double newHeight,
+                  double newWeight);
 
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
-  Person person1;
-  person1.name = "Spongebob Squarepants";
-  person1.age = 11;
-  person1.height = 1.22;
+  Person person1 = {"Spongebob Squarepants", 11, 1.22, 65.25};
+  Person person2 = {"Patrick Star", 12, 1.33, 75.25};
 
+  std::cout << "Initial information:" << std::endl;
   printPerson(person1);
+  printPerson(person2);
+
+  updatePerson(person1, 12, 1.25, 67.5);
+  updatePerson(person2, 13, 1.35, 77.0);
+
+  std::cout << "\nUpdated information:" << std::endl;
+  printPerson(person1);
+  printPerson(person2);
 
   return 0;
 }
 
-void printPerson(Person &person) {
+void printPerson(const Person &person) {
   std::cout << "Name: " << person.name << std::endl;
   std::cout << "Age: " << person.age << " years" << std::endl;
-  std::cout << "Height: " << person.height << " meters" << std::endl;
+  std::cout << "Height: " << person.height << " m" << std::endl;
+  std::cout << "Weight: " << person.weight << " kg" << std::endl;
+  std::cout << std::endl;
+}
+
+void updatePerson(Person &person, int newAge, double newHeight,
+                  double newWeight) {
+  person.age = newAge;
+  person.height = newHeight;
+  person.weight = newWeight;
 }
