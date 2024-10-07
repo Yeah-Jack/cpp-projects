1d9dfaf82919edbdee97eea95e7e5b7ab18e1528
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-10-07 08:58:04 +0000

    feat: add program to calculate the sum between two numbers

---
+++ English/SumBetweenNumbers/main.cpp
@@ -0,0 +1,23 @@
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+
+	int num1;
+	int num2;
+	int result = 0;
+
+	std::cout << "Enter the first number: ";
+	std::cin >> num1;
+	std::cout << "Enter the second number: ";
+	std::cin >> num2;
+
+	for (int i = num1; i <= num2; i++) {
+		result += i;
+	}
+
+	std::cout << result << '\n';
+
+	return 0;
+}
