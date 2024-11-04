f760936918c49ae6314bdcaaa486f392e502ee71
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-11-04 10:43:10 +0100

    feat: add program to find and display divisors of a number

---
+++ English/NumberDivisor/main.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+
+	int number;
+	int divisor;
+
+	std::cout << "Enter a number: ";
+	std::cin >> number;
+
+	std::cout << "Divisors of " << number << ": ";
+	for (divisor = 1; divisor <= number; divisor++) {
+		if (number % divisor == 0) {
+			std::cout << divisor << " ";
+		}
+	}
+
+	return 0;
+}
