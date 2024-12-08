d1b232d8e370f9b3b91b054eb571329a2bd03216
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-12-08 17:29:47 +0100

    feat: add options to print numbers and even numbers using separate functions

--- English/ForExercises/main.cpp
+++ English/ForExercises/main.cpp
@@ -1,19 +1,51 @@
-// Write a program that uses a for loop to print the numbers from 1 to 10.
-
 #include <iostream>
 #include <windows.h>
 
+void PrintNumbersUpTo();
+void PrintEvenNumbersUpTo();
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
-  int count;
+  short choice;
 
-  std::cout << "Who many times do you want to repeat the loop?\n";
-  std::cin >> count;
+  std::cout << "Which program do you want to run? 1 or 2?\n";
+  std::cin >> choice;
 
-  for (int i = 0; i < count; i++) {
-    std::cout << i + 1 << '\n';
+  switch (choice) {
+  case 1:
+    PrintNumbersUpTo();
+    break;
+  case 2:
+    PrintEvenNumbersUpTo();
+    break;
+  default:
+    std::cout << "You didn't enter a valid number.\n";
+    break;
   }
 
   return 0;
+}
+
+void PrintNumbersUpTo() {
+  // Write a program that uses a for loop to print the numbers from 1 to 10.
+  int number;
+
+  std::cout << "Enter the number up to which you want to print: ";
+  std::cin >> number;
+
+  for (int i = 1; i <= number; ++i) {
+    std::cout << i << '\n';
+  }
+}
+
+void PrintEvenNumbersUpTo() {
+  // Create a program that prints all even numbers between 1 and N.
+  int number;
+
+  std::cout << "Enter the number up to which you want to print: ";
+  std::cin >> number;
+
+  for (int i = 2; i <= number; i += 2) {
+    std::cout << i << '\n';
+  }
 }
