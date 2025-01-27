e72189c6845bc8cb860031c74a04b49db33769b8
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-01-27 10:25:36 +0100

    refactor: simplify condition checks in resistor calculation logic using DeMorgan's Theorem

--- GUI/German/Widerstandsrechner/widget.cpp
+++ GUI/German/Widerstandsrechner/widget.cpp
@@ -13,17 +13,17 @@ void Widget::on_berechnen_clicked() {
   double widerstand3 = 0;
   double ergebnis;
 
-  if (ui->btnParallel2R->isChecked() == true) {
+  if (ui->btnParallel2R->isChecked()) {
     widerstand1 = ui->widerstand1->value();
     widerstand2 = ui->widerstand2->value();
     ergebnis = (widerstand1 * widerstand2) / (widerstand1 + widerstand2);
   }
-  if (ui->btnReihe2R->isChecked() == true) {
+  if (ui->btnReihe2R->isChecked()) {
     widerstand1 = ui->widerstand1->value();
     widerstand2 = ui->widerstand2->value();
     ergebnis = widerstand1 + widerstand2;
   }
-  if (ui->btnReihe3R->isChecked() == true) {
+  if (ui->btnReihe3R->isChecked()) {
     widerstand1 = ui->widerstand1->value();
     widerstand2 = ui->widerstand2->value();
     widerstand3 = ui->widerstand3->value();
