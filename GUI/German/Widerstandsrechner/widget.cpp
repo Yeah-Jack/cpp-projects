8a1932bf28735e995a1191b8c588b230a931851b
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-01-27 10:17:35 +0100

    feat: implement main functionality and UI for German resistor calculator

---
+++ GUI/German/Widerstandsrechner/widget.cpp
@@ -0,0 +1,33 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
+  ui->setupUi(this);
+}
+
+Widget::~Widget() { delete ui; }
+
+void Widget::on_berechnen_clicked() {
+  double widerstand1 = 0;
+  double widerstand2 = 0;
+  double widerstand3 = 0;
+  double ergebnis;
+
+  if (ui->btnParallel2R->isChecked() == true) {
+    widerstand1 = ui->widerstand1->value();
+    widerstand2 = ui->widerstand2->value();
+    ergebnis = (widerstand1 * widerstand2) / (widerstand1 + widerstand2);
+  }
+  if (ui->btnReihe2R->isChecked() == true) {
+    widerstand1 = ui->widerstand1->value();
+    widerstand2 = ui->widerstand2->value();
+    ergebnis = widerstand1 + widerstand2;
+  }
+  if (ui->btnReihe3R->isChecked() == true) {
+    widerstand1 = ui->widerstand1->value();
+    widerstand2 = ui->widerstand2->value();
+    widerstand3 = ui->widerstand3->value();
+    ergebnis = widerstand1 + widerstand2 + widerstand3;
+  }
+  ui->ergebnis->display(ergebnis);
+}
