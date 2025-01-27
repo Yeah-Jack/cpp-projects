9055d0cc568a485d558a25abd98ba867ae3228f0
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-01-27 13:17:42 +0100

    feat: enhance change calculator to include banknotes and improve output

--- Console/German/Wechselgeldrechner/main.cpp
+++ Console/German/Wechselgeldrechner/main.cpp
@@ -1,35 +1,34 @@
-// Erstellen Sie ein Programm, das die Anzahl der
-// Wechselgeldmünzen berechnet. Der Benutzer gibt den zu zahlenden Betrag und
-// den gegebenen Betrag an, das Programm berechnet das Rückgeld und die Anzahl
-// der Münzen. Es soll möglichst wenige Münzen ausgeben.
+// Erstelle ein Programm, das die Anzahl der Wechselgeldmünzen berechnet. Der
+// Benutzer gibt den zu zahlenden Betrag und den gegebenen Betrag an, das
+// Programm berechnet das Rückgeld und die Anzahl der Münzen und Scheinen. Es
+// sollen möglichst wenige Münzen und Scheine ausgeben werden.
 
+#include <cmath>
 #include <iostream>
 #include <windows.h>
 
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
-  const double einCent = 0.01;
-  const double zweiCent = 0.02;
-  const double fuenfCent = 0.05;
-  const double zehnCent = 0.10;
-  const double zwanzigCent = 0.20;
-  const double fuenfzigCent = 0.50;
-  const double einEuro = 1.00;
-  const double zweiEuro = 2.00;
-
   double zuZahlenderBetrag;
   double gegebenerBetrag;
   double rueckgeld;
+  int rueckgeldInCents;
 
-  int anzahlEinCent;
-  int anzahlZweiCent;
-  int anzahlFuenfCent;
-  int anzahlZehnCent;
-  int anzahlZwanzigCent;
-  int anzahlFuenfzigCent;
-  int anzahlEinEuro;
-  int anzahlZweiEuro;
+  int anzahlEinCent = 0;
+  int anzahlZweiCent = 0;
+  int anzahlFuenfCent = 0;
+  int anzahlZehnCent = 0;
+  int anzahlZwanzigCent = 0;
+  int anzahlFuenfzigCent = 0;
+  int anzahlEinEuro = 0;
+  int anzahlZweiEuro = 0;
+  int anzahlFuenfEuro = 0;
+  int anzahlZehnEuro = 0;
+  int anzahlZwanzigEuro = 0;
+  int anzahlFuenfzigEuro = 0;
+  int anzahlHundertEuro = 0;
+  int anzahlZweihundertEuro = 0;
 
   std::cout << "Gebe den zu zahlenden Betrag in € ein: ";
   std::cin >> zuZahlenderBetrag;
@@ -38,39 +37,92 @@ int main() {
   std::cin >> gegebenerBetrag;
 
   rueckgeld = gegebenerBetrag - zuZahlenderBetrag;
+  rueckgeldInCents = static_cast<int>(rueckgeld * 100);
+  std::cout << "Dein Rückgeld beträgt: " << rueckgeld << "€.\n";
 
-  std::cout << "Das Rückgeld beträgt: " << rueckgeld << "€.\n";
+  while (rueckgeldInCents >= 20000) {
+    rueckgeldInCents -= 20000;
+    anzahlZweihundertEuro++;
+  }
 
-  anzahlZweiEuro = rueckgeld / zweiEuro;
-  rueckgeld -= anzahlZweiEuro * zweiEuro;
+  while (rueckgeldInCents >= 10000) {
+    rueckgeldInCents -= 10000;
+    anzahlHundertEuro++;
+  }
+
+  while (rueckgeldInCents >= 5000) {
+    rueckgeldInCents -= 5000;
+    anzahlFuenfzigEuro++;
+  }
+
+  while (rueckgeldInCents >= 2000) {
+    rueckgeldInCents -= 2000;
+    anzahlZwanzigEuro++;
+  }
+
+  while (rueckgeldInCents >= 1000) {
+    rueckgeldInCents -= 1000;
+    anzahlZehnEuro++;
+  }
+
+  while (rueckgeldInCents >= 500) {
+    rueckgeldInCents -= 500;
+    anzahlFuenfEuro++;
+  }
+
+  while (rueckgeldInCents >= 200) {
+    rueckgeldInCents -= 200;
+    anzahlZweiEuro++;
+  }
+
+  while (rueckgeldInCents >= 100) {
+    rueckgeldInCents -= 100;
+    anzahlEinEuro++;
+  }
+
+  while (rueckgeldInCents >= 50) {
+    rueckgeldInCents -= 50;
+    anzahlFuenfzigCent++;
+  }
+
+  while (rueckgeldInCents >= 20) {
+    rueckgeldInCents -= 20;
+    anzahlZwanzigCent++;
+  }
+
+  while (rueckgeldInCents >= 10) {
+    rueckgeldInCents -= 10;
+    anzahlZehnCent++;
+  }
+
+  while (rueckgeldInCents >= 5) {
+    rueckgeldInCents -= 5;
+    anzahlFuenfCent++;
+  }
+
+  while (rueckgeldInCents >= 2) {
+    rueckgeldInCents -= 2;
+    anzahlZweiCent++;
+  }
+
+  while (rueckgeldInCents >= 1) {
+    rueckgeldInCents -= 1;
+    anzahlEinCent++;
+  }
+
+  std::cout << "Anzahl 200€ Scheine: " << anzahlZweihundertEuro << '\n';
+  std::cout << "Anzahl 100€ Scheine: " << anzahlHundertEuro << '\n';
+  std::cout << "Anzahl 50€ Scheine: " << anzahlFuenfzigEuro << '\n';
+  std::cout << "Anzahl 20€ Scheine: " << anzahlZwanzigEuro << '\n';
+  std::cout << "Anzahl 10€ Scheine: " << anzahlZehnEuro << '\n';
+  std::cout << "Anzahl 5€ Scheine: " << anzahlFuenfEuro << '\n';
   std::cout << "Anzahl 2€ Münzen: " << anzahlZweiEuro << '\n';
-
-  anzahlEinEuro = rueckgeld / einEuro;
-  rueckgeld -= anzahlEinEuro * einEuro;
   std::cout << "Anzahl 1€ Münzen: " << anzahlEinEuro << '\n';
-
-  anzahlFuenfzigCent = rueckgeld / fuenfzigCent;
-  rueckgeld -= anzahlFuenfzigCent * fuenfzigCent;
   std::cout << "Anzahl 50c Münzen: " << anzahlFuenfzigCent << '\n';
-
-  anzahlZwanzigCent = rueckgeld / zwanzigCent;
-  rueckgeld -= anzahlZwanzigCent * zwanzigCent;
   std::cout << "Anzahl 20c Münzen: " << anzahlZwanzigCent << '\n';
-
-  anzahlZehnCent = rueckgeld / zehnCent;
-  rueckgeld -= anzahlZehnCent * zehnCent;
   std::cout << "Anzahl 10c Münzen: " << anzahlZehnCent << '\n';
-
-  anzahlFuenfCent = rueckgeld / fuenfCent;
-  rueckgeld -= anzahlFuenfCent * fuenfCent;
   std::cout << "Anzahl 5c Münzen: " << anzahlFuenfCent << '\n';
-
-  anzahlZweiCent = rueckgeld / zweiCent;
-  rueckgeld -= anzahlZweiCent * zweiCent;
   std::cout << "Anzahl 2c Münzen: " << anzahlZweiCent << '\n';
-
-  anzahlEinCent = rueckgeld / einCent;
-  rueckgeld -= anzahlEinCent * einCent;
   std::cout << "Anzahl 1c Münzen: " << anzahlEinCent << '\n';
 
   return 0;
