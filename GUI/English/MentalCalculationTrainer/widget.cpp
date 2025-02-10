979cd0fc9a257db6d85fe64d1d50a2d7ab85027b
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-02-10 11:09:08 +0100

    feat: implement basic Mental Calculation Trainer UI with arithmetic operations

---
+++ GUI/English/MentalCalculationTrainer/widget.cpp
@@ -0,0 +1,31 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
+  ui->setupUi(this);
+}
+
+Widget::~Widget() { delete ui; }
+
+void Widget::on_calculate_clicked() {
+  switch (ui->calculationType->currentIndex()) {
+  case 0:
+      ui->result->display(ui->number1->value() + ui->number2->value());
+      break;
+  case 1:
+      ui->result->display(ui->number1->value() - ui->number2->value());
+      break;
+  case 2:
+      ui->result->display(ui->number1->value() * ui->number2->value());
+      break;
+  case 3:
+      ui->result->display(ui->number1->value() / ui->number2->value());
+      break;
+  }
+
+  if (ui->userResult->value() == ui->result->value()) {
+      ui->user->setText("You're right");
+  } else {
+      ui->user->setText("You're wrong");
+  }
+}
