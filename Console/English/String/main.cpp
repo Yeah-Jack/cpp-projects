a86d0a1dc5c7325022cd0c7bfb4e52c75d9d8660
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-02-17 10:37:36 +0100

    feat: add string search and insertion functionality to string program

--- Console/English/String/main.cpp
+++ Console/English/String/main.cpp
@@ -3,12 +3,38 @@
 // the character appears several times, all digits (separated by spaces) should
 // be output.
 
+// Create a program that reads in a string. Subsequently, a second text is to be
+// read in. Finally, the position at which the text is to be inserted is to be
+// read in. text is to be inserted.
+
 #include <iostream>
 #include <windows.h>
 
+void searchString();
+void insertString();
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
+  short choice;
+  std::cout << "1) Search a character in a string\n";
+  std::cout << "2) Insert a string in a string\n";
+  std::cout << "Enter your choice: ";
+  std::cin >> choice;
+  std::cin.ignore(); // Clear the newline character from the input buffer
+
+  switch (choice) {
+  case 1:
+    searchString();
+    break;
+  case 2:
+    insertString();
+    break;
+  }
+
+  return 0;
+}
+
+void searchString() {
   std::string text;
   size_t position;
   char search;
@@ -29,6 +55,24 @@ int main() {
     position = text.find(search, position + 1);
   }
   std::cout << '\n';
+}
 
-  return 0;
+void insertString() {
+  std::string text;
+  std::string text1;
+  std::string text2;
+  size_t position;
+
+  std::cout << "Enter the first text: ";
+  std::getline(std::cin, text1);
+  std::cout << "Enter the second text: ";
+  std::getline(std::cin, text2);
+
+  std::cout << "The second text has to be appended at character "
+            << text1.size()
+            << "\nAt which character do you want to insert the second text? ";
+  std::cin >> position;
+  text = text1;
+  text.insert(position, text2);
+  std::cout << "The new text is: " << text << '\n';
 }
