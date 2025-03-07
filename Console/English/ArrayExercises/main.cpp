de9ed17ca3b009b9d295a5d98f3f5eb9cfdfb8e4
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-03-07 20:43:57 +0100

    feat: allow user input for integer array and calculate sum, average, largest, and smallest values

--- Console/English/ArrayExercises/main.cpp
+++ Console/English/ArrayExercises/main.cpp
@@ -1,3 +1,8 @@
+// Let the user input its own variables. Declare, initialize, and print all
+// elements of an integer array. Calculate the sum and average of elements in an
+// array. Identify the largest and smallest values in an array. Reverse the
+// order of elements and print the result.
+
 #include <algorithm>
 #include <iostream>
 #include <windows.h>
@@ -6,20 +11,26 @@ void sort(int numbers[], int size);
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
-  int numbers[6] = {5, 3, 2, 6, 4, 1};
+  int numbers[8] = {5, 3, 2, 6, 4, 1};
+
+  for (int i = 6; i < 8; i++) {
+    std::cout << "Enter a number: ";
+    std::cin >> numbers[i];
+  }
+
   int size = sizeof(numbers) / sizeof(numbers[0]);
   int sum = 0;
   double average = 0.0;
 
   sort(numbers, size);
-  std::cout << "Sorted array: ";
+  std::cout << "\nSorted array: ";
   for (int number : numbers) {
     std::cout << number << ' ';
     sum += number;
   }
+  average = static_cast<double>(sum) / size;
 
   std::cout << "\nSum: " << sum << '\n';
-  average = sum / size;
   std::cout << "Average: " << average << '\n';
   std::cout << "Smallest number: " << numbers[0] << '\n';
   std::cout << "Largest number: " << numbers[size - 1] << '\n';
