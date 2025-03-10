d0f975cf5f4637bc79ac6d38d483344db1a649ff
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-03-10 10:51:59 +0100

    refactor: optimize student data initialization using a loop

--- Console/English/StudentDataManagement/main.cpp
+++ Console/English/StudentDataManagement/main.cpp
@@ -18,25 +18,11 @@ int main() {
   // and the third stores their grades.
   int students[5][3];
 
-  students[0][0] = 1;
-  students[0][1] = age(random);
-  students[0][2] = grade(random);
-
-  students[1][0] = 2;
-  students[1][1] = age(random);
-  students[1][2] = grade(random);
-
-  students[2][0] = 3;
-  students[2][1] = age(random);
-  students[2][2] = grade(random);
-
-  students[3][0] = 4;
-  students[3][1] = age(random);
-  students[3][2] = grade(random);
-
-  students[4][0] = 5;
-  students[4][1] = age(random);
-  students[4][2] = grade(random);
+  for (int i = 0; i < 5; i++) {
+    students[i][0] = i + 1;
+    students[i][1] = age(random);
+    students[i][2] = grade(random);
+  }
 
   printStudentData(students);
 
