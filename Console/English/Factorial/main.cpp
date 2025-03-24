dc69cc4962814684564e54d6e3c64952c4edf5cb
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2025-03-24 10:45:07 +0100

    feat: implement factorial calculation with iterative and recursive methods

--- Console/English/Factorial/main.cpp
+++ Console/English/Factorial/main.cpp
@@ -4,26 +4,47 @@
 #include <iostream>
 #include <windows.h>
 
-long factorialIterative(long &number);
-// long factorialRecursive(long &number);
+long factorialIterative(long number);
+long factorialRecursive(long number);
 
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
   long number;
+  short choice;
 
   std::cout << "Enter a number: ";
   std::cin >> number;
 
-  std::cout << "Factorial of your number is: " << factorialIterative(number)
-            << '\n';
-  // std::cout << "Factorial of your number is: " << factorialRecursive(number)
-  //           << '\n';;
+  if (number < 0) {
+    std::cout << "Factorial is not defined for negative numbers.\n";
+    return 1;
+  }
+
+  std::cout << "Choose a method to calculate the factorial\n1) Iterative\n2) "
+               "Recursive\n";
+  std::cin >> choice;
+
+  switch (choice) {
+  case 1:
+    std::cout << "Factorial of your number is: " << factorialIterative(number)
+              << '\n';
+    break;
+
+  case 2:
+    std::cout << "Factorial of your number is: " << factorialRecursive(number)
+              << '\n';
+    break;
+
+  default:
+    std::cout << "Invalid choice\n";
+    break;
+  }
 
   return 0;
 }
 
-long factorialIterative(long &number) {
+long factorialIterative(long number) {
   long result = 1;
 
   for (int i = 1; i <= number; i++) {
@@ -33,10 +54,10 @@ long factorialIterative(long &number) {
   return result;
 }
 
-// long factorialRecursive(long &number) {
-//   if (number > 1) {
-//     return number * factorialRecursive(number - 1);
-//   } else {
-//     return 1;
-//   }
-// }
+long factorialRecursive(long number) {
+  if (number > 1) {
+    return number * factorialRecursive(number - 1);
+  } else {
+    return 1;
+  }
+}
