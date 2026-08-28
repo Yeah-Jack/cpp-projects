9c4cdae0b74570cd1ead4707e2b94dc2f46b19db
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2026-08-28 10:58:22 +0200

    Add inital Client Server Broadcast app

---
+++ GUI/English/ClientServerBroadcast/widget.cpp
@@ -0,0 +1,32 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(
+    QWidget *parent)
+    : QWidget(parent), ui(new Ui::Widget) {
+  ui->setupUi(this);
+  mySocket = new QTcpSocket(this);
+}
+
+Widget::~Widget() { delete ui; }
+
+void Widget::on_btnConnectServer_clicked() {
+  mySocket->connectToHost("localhost", 4711);
+  QObject::connect(mySocket, &QTcpSocket::readyRead, this, &Widget::readData);
+}
+
+void Widget::on_btnSendText_clicked() {
+  QString sendText;
+
+  sendText = ui->edtSendText->text();
+
+  mySocket->write(sendText.toLatin1());
+  qDebug() << sendText;
+}
+
+void Widget::readData() {
+  QString receivedText;
+
+  receivedText = mySocket->readLine();
+  ui->edtReceiveText->appendPlainText(receivedText);
+}
